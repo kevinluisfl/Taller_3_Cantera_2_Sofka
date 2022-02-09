dac4e33339757e133a823b8a02c95ad564019592
kevinluisfl <71282006+kevinluisfl@users.noreply.github.com>
2022-02-09 15:33:19 -0500

    inicio proyecto

---
+++ src/tallerpractico3/TallerPractico3.java
@@ -0,0 +1,17 @@
+
+package tallerpractico3;
+
+/**
+ *
+ * @author kevinf
+ */
+public class TallerPractico3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
