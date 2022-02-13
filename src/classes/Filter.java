d99e60b412ba18835823bd9289c6766869fbc2c8
Juan David <juandavid-1527@hotmail.com>
2022-02-13 17:24:26 -0500

    Se comienza con la documentación

--- src/classes/Filter.java
+++ src/classes/Filter.java
@@ -1,16 +1,31 @@
+/**
+ * Se importa el paquete y librerias a usar.
+ */
 package classes;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
+/**
+ * Representa la clase Filter con implementación de la interfaz IFilter.
+ * @authores kevin y Juan David.
+ */
 public class Filter implements IFilter {
     ArrayList<Song> libraryFilter = new ArrayList<>();
     
     SimpleDateFormat getYearFormat = new SimpleDateFormat("yyyy");
 
+    /**
+     * Hereda el metodo FilterYear de la interfaz IFilter.
+     * @param list
+     * @param year
+     * @return ArrayList del metodo FilterYear.
+     */
     @Override
     public ArrayList<Song> FilterYear(ArrayList<Song> list, int year) {
-
+/**
+ * Representa un metodo para filtrar las canciones por año.
+ */
         for (Song item: list) {
             int yearSong = Integer.parseInt (getYearFormat.format(item.getDate()));
             if (yearSong == year) {
@@ -20,9 +35,17 @@ public class Filter implements IFilter {
         return libraryFilter;
     }
 
+    /**
+     * Hereda el metodo FilterGenre de la interfaz IFilter.
+     * @param list
+     * @param genre
+     * @return ArrayList del metodo FilterGenre
+     */
     @Override
     public ArrayList<Song> FilterGenre(ArrayList<Song> list, String genre) {
-
+/**
+ * Representa el metodo para filtrar canciones por genero.
+ */
         for (Song item: list) {
             if (item.getGenre() == genre) {
                 libraryFilter.add(item);
--- src/classes/Song.java
+++ src/classes/Song.java
@@ -1,7 +1,14 @@
+/**
+ * Se importa el paquete classes y la libreria Date.
+ */
 package classes;
 
 import java.util.Date;
 
+/**
+ * Representa la clase Song.
+ * @authores Kevin y Juan David.
+ */
 public class Song {
     private String title;
     private Integer identifier;
@@ -15,6 +22,13 @@ public class Song {
 
     }
 
+    /**
+     * Representa el constructor de la clase Song.
+     * @param title
+     * @param date
+     * @param duration
+     * @param genre
+     */
     public Song(String title, Date date, int duration, String genre) {
         this.title = title;
         this.date = date;
@@ -32,66 +46,130 @@ public class Song {
         this.description = description;
     }
 
+    /**
+     * Representa el metodo getTitle.
+     * @return titile.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Representa el metodo setTitles.
+     * @param title
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Representa el metodo getIdentifier.
+     * @return identifier
+     */
     public int getIdentifier() {
         return identifier;
     }
 
+    /**
+     * Representa el metodo setIdentifier.
+     * @param identifier
+     */
     public void setIdentifier(int identifier) {
         this.identifier = identifier;
     }
 
+    /**
+     * Rerpesenta el metodo getDate.
+     * @return date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * Representa el metodo setDate.
+     * @param date
+     */
     public void setDate(Date date) {
         this.date = date;
     }
 
+    /**
+     * Representa el metodo getDuration.
+     * @return duration
+     */
     public int getDuration() {
         return duration;
     }
 
+    /**
+     * Representa el metodo setDuration.
+     * @param duration
+     */
     public void setDuration(int duration) {
         this.duration = duration;
     }
 
+    /**
+     * Representa el metodo setGenre.
+     * @return genre
+     */
     public String getGenre() {
         return genre;
     }
 
+    /**
+     * Representa el metodo setGenre.
+     * @param genre
+     */
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
+    /**
+     * Representa el metodo getCover
+     * @return cover
+     */
     public String getCover() {
         return cover;
     }
 
+    /**
+     * Representa el metodo cover.
+     * @param cover
+     */
     public void setCover(String cover) {
         this.cover = cover;
     }
 
+    /**
+     * Representa el metodo getDescription.
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Representa el metodo setDescription.
+     * @param description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
-    
+
+    /**
+     * Representa el metodo getMillis.
+     * @return this.date.getTime
+     */
     public long getMillis(){
        return this.date.getTime();
     }
 
+    /**
+     * Hereda atributos de la clase Song.
+     * @return title, identifier, date, duration, genre, cover, description
+     */
     @Override
     public String toString() {
         return "Titulo='" + title + '\'' +
